8179771fff106d27ccd23e52cae033f907339d22
sjy <ks2414e@naver.com>
2021-02-26 20:44:44 +0900

    use multer utils

--- routes/multer.js
+++ routes/multer.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { MulterController : controller } = require('../controllers');
-const multer = require('multer');
-const upload = multer({ dest : './uploads'}).fields([{name:'img'},{name:'img2'}]);
-
-router.use('/img', express.static('./uploads'));
+const { upload } = require('../utils/multer');
 
 router.get('/list', controller.List);
 router.post('/upload', upload, controller.Add);
