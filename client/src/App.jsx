b0762b9441eff790836ff26b7a16ad6c2c1a2df6
sjy <ks2414e@naver.com>
2021-03-07 19:28:49 +0900

    plus single&array

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,76 +1,175 @@
-import {Component} from 'react';
-import './App.css';
-import axios from 'axios';
-import List from './component/List';
+import { Component } from "react";
+import "./App.css";
+import axios from "axios";
+import List from "./component/List";
 
 class App extends Component {
-
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      file : '',
-      filename : '',
-      file2 : '',
-      filename2 : '',
-      imgarray : []
-    }
+      file: "",
+      filename: "",
+      file2: "",
+      filename2: "",
+      singlefile: "",
+      arrayfile: [],
+      imgsingle: [],
+      imgarray: [],
+      imgfields: [],
+    };
   }
-  FileChange = (e) => {
+
+  SingleChange = (e) => {
     this.setState({
-        file : e.target.files[0],
-        filename : e.target.value
-    });
-  }
-  FileChange2 = (e) => {
-    this.setState({
-        file2 : e.target.files[0],
-        filename2 : e.target.value
+      singlefile: e.target.files[0],
     });
   };
 
-  upload = async (e) => {
+  FileChange = (e) => {
+    this.setState({
+      file: e.target.files[0],
+      filename: e.target.value,
+    });
+  };
+  FileChange2 = (e) => {
+    this.setState({
+      file2: e.target.files[0],
+      filename2: e.target.value,
+    });
+  };
+
+  ArrayChange = (e) => {
+    this.setState({ arrayfile: e.target.files });
+  };
+
+  fieldsupload = async (e) => {
     e.preventDefault();
     const formData = new FormData();
-    formData.append('img', this.state.file);
-    formData.append('img2', this.state.file2);
+    formData.append("img", this.state.file);
+    formData.append("img2", this.state.file2);
 
-    await axios.post('http://localhost:8081/api/multer/upload', formData);
+    await axios.post("/api/multer/fieldsupload", formData);
 
-    alert('uploading....');
+    alert("uploading....");
     window.location.reload();
   };
 
-  componentDidMount(){
-    axios.get('http://localhost:8081/api/multer/list')
-      .then(res => {
-        this.setState({
-          imgarray : res.data
-        });
-      });
+  singleupload = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("img", this.state.singlefile);
+    await axios
+      .post("/api/multer/singleupload", formData)
+      .then((res) => console.log(res));
+
+    alert("uploading....");
+    window.location.reload();
   };
 
-  render(){
-  return (
-    <div className="App">
-    <h1>Upload Files</h1>
-    <input type="file" name="file" file={this.state.file} value={this.state.filename} onChange={this.FileChange}/>
-    <input type="file" name="file2" file={this.state.file2} value={this.state.filename2} onChange={this.FileChange2}/>  
-    <button onClick={this.upload}>uplaod</button>
-    <hr/>
-    <h1>Upload File List</h1>
-    {
-      this.state.imgarray ?
-      this.state.imgarray.map(k => {
-        return (<List
-          key = {k.id}
-          img = {k.img}
-          img2 = {k.img2}
-          />)
-      }) : "DB NOT CONNECT"
+  arrayupload = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    for (const file of this.state.arrayfile) {
+      formData.append("imgs", file);
     }
-    </div>
-    );
+    await axios
+      .post("/api/multer/arrayupload", formData)
+      .catch((err) => console.log(err));
   };
-};
+
+  async componentDidMount() {
+    await axios.get("/api/multer/singlelist").then((res) => {
+      this.setState({
+        imgsingle: res.data,
+      });
+    });
+    await axios.get("/api/multer/fieldslist").then((res) => {
+      this.setState({
+        imgfields: res.data,
+      });
+    });
+    await axios.get("/api/multer/arraylist").then((res) => {
+      let arr_Data = [];
+      for (let i = 0; i < res.data.length; i++) {
+        for (let j = 0; j < res.data[i].img.split(",").length; j++) {
+          arr_Data.push(res.data[i].img.split(",")[j]);
+        }
+      }
+      this.setState({
+        arrayfile: arr_Data,
+      });
+    });
+  }
+
+  render() {
+    let imgobj = this.state.arrayfile;
+    let imglength = this.state.arrayfile.length;
+    return (
+      <div className="App">
+        <h1>Upload Files_Single</h1>
+        <input
+          type="file"
+          name="file"
+          file={this.state.singlefile}
+          onChange={this.SingleChange}
+        />
+        <button onClick={this.singleupload}>uplaod</button>
+        <hr />
+
+        <h1>Upload Files_Fields</h1>
+        <input
+          type="file"
+          name="file"
+          file={this.state.file}
+          value={this.state.filename}
+          onChange={this.FileChange}
+        />
+        <input
+          type="file"
+          name="file2"
+          file={this.state.file2}
+          value={this.state.filename2}
+          onChange={this.FileChange2}
+        />
+        <button onClick={this.fieldsupload}>uplaod</button>
+        <hr />
+        <h1>Upload Files_Array</h1>
+        <input
+          type="file"
+          multiple
+          file={this.state.arrayfile}
+          onChange={this.ArrayChange}
+        />
+        <button onClick={this.arrayupload}>uplaod</button>
+        <hr />
+
+        <h1>Upload File List_single</h1>
+        {this.state.imgsingle
+          ? this.state.imgsingle.map((k) => {
+              return <List key={k.id} img={k.img} />;
+            })
+          : "DB NOT CONNECT"}
+        <hr />
+        <h1>Upload File List_fields</h1>
+        {this.state.imgfields
+          ? this.state.imgfields.map((k) => {
+              return <List key={k.id} img={k.img} img2={k.img2} />;
+            })
+          : "DB NOT CONNECT"}
+        <hr />
+        <h1>Upload File List_Array</h1>
+        {(function () {
+          let tagarray = [];
+          for (let i = 0; i < imglength; i++) {
+            let img = <img src={imgobj[i]} width={200} height={200} alt="" />;
+            console.log(img);
+            tagarray.push(img);
+          }
+          return tagarray;
+        })()}
+      </div>
+    );
+  }
+}
 
 export default App;
